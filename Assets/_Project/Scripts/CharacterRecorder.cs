872b6795f1a1f7d5abc565e11f500820be715420
tobia88 <tobia88@gmail.com>
2021-01-30 15:24:41 +0800

    Update Player Prefab

---
+++ Assets/_Project/Scripts/CharacterRecorder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
+
+public class CharacterRecorder : MonoBehaviour
+{
+    public float timeSnapInterval = 0.5f;
+
+    public Queue<SnapData> snapDatas = new Queue<SnapData>();
+
+    private void Start()
+    {
+        StartCoroutine(SnapperUpdate());
+    }
+
+    IEnumerator SnapperUpdate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeSnapInterval);
+            snapDatas.Enqueue(new SnapData(){ position = transform.position, rotation = transform.rotation});
+        }
+    }
+}
